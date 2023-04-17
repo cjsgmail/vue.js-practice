27cffa4479e23c65973e92ca84e6395ea86df3f5
cjsgmail <cjs0097@gmail.com>
2023-04-17 20:08:28 +0900

    Vue.js 학습

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="modal-bg" v-if="modalOpen === true" @click="modalOpen = false">
+    <div class="modal-content">
+      <h4>상세페이지</h4>
+      <p>상세페이지</p>
+    </div>
+  </div>
+
+  <div class="tab">
+    <a v-for="(el, i) in tabs" :key="i">{{ el }}</a>
+  </div>
+
+  <div v-for="(el, i) in roomData" :key="i" @click="modalOpen = true">
+    <img :src="el.image" class="room-img" />
+    <h4>{{ el.title }}</h4>
+    <p>월 {{ el.price }}원</p>
+    <button @click="report[i]++">허위매물신고</button>
+    <span>신고수 : {{ report[i] }}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import roomData from "./assets/roomData";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      modalOpen: false,
+      tabs: ["Home", "Shop", "About"],
+      report: [0, 0, 0],
+      roomData: roomData,
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
@@ -21,6 +43,47 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.tab {
+  background-color: purple;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.tab a {
+  color: white;
+}
+
+.room-img {
+  width: 70%;
+  margin-top: 40px;
+}
+
+.modal-bg {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 30px;
+}
+
+.modal-content {
+  position: relative;
+  top: 30%;
+  width: 100%;
+  border-radius: 20px;
+  background-color: white;
+  padding: 30px;
 }
 </style>
